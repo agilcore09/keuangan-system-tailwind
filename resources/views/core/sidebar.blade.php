f9106d1d64ebe97d35cb9aad09bf7f8137803674
unknown <agiljibrin009@gmail.com>
2023-11-18 21:30:03 +0800

    test

---
+++ resources/views/core/sidebar.blade.php
@@ -0,0 +1,12 @@
+<div class="sidebar w-44  bg-blue-950 h-screen shadow-xl">
+    <div class="sidebar-head d-flex justify-center w-full">
+
+        <img src="{{ asset('/logo.png') }}" alt="Logo Persada" class="w-20">
+        <div style="border: 1px solid white; width: 100%;" class="mt-5"></div>
+        sakjdkajsdkjakjdjakjsdsd
+        jdkajskdjakjdskjakjdk
+        jaksjdkaksjdkjajdkajd
+        gdagsdhsahaddsadsadasdsadad
+        ddasdasdasads
+    </div>
+</div>
